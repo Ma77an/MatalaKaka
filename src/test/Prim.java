24a1bf92385be39ddc1a59e05ea27b4b07e175dc
Matan Gabso <matang99@gmail.com>
2021-12-11 16:06:54 +0200

    קוד סופי סופי סופי סופי להגשה זהו הגשנו ביי להתראות

--- src/test/Prim.java
+++ src/test/Prim.java
@@ -12,11 +12,7 @@ class Prim {
     private static final int V = 20;
     int[][] tree = new int[V][V];
 
-
-    // A utility function to find the vertex with minimum key
-    // value, from the set of vertices not yet included in MST
     int minKey(int[] key, Boolean[] mstSet) {
-        // Initialize min value
         int min = Integer.MAX_VALUE, min_index = -1;
 
         for (int v = 0; v < V; v++)
@@ -28,8 +24,6 @@ class Prim {
         return min_index;
     }
 
-    // A utility function to print the constructed MST stored in
-    // pi[]
     void builderMst(int[][] tree, int[] pi, int[][] graph) {
         for (int[] row : tree) {
             Arrays.fill(row, -1);
@@ -57,20 +51,15 @@ class Prim {
         }
     }
 
-    // Array to store constructed MST
     int[] pi = new int[V];
 
-    // Function to construct and print MST for a graph represented
-    // using adjacency matrix representation
     void primMST(int[][] graph) {
 
-        // Key values used to pick minimum weight edge in cut
+
         int[] key = new int[V];
 
-        // To represent set of vertices included in MST
         Boolean[] mstSet = new Boolean[V];
 
-        // Initialize all keys as INFINITE
         for (int i = 0; i < V; i++) {
             key[i] = Integer.MAX_VALUE;
             mstSet[i] = false;
@@ -86,37 +75,20 @@ class Prim {
 
         System.out.println("\n**************************************\n");
 
+        key[0] = 0;
+        pi[0] = -1;
 
-        // Always include first 1st vertex in MST.
-        key[0] = 0; // Make key 0 so that this vertex is
-        // picked as first vertex
-        pi[0] = -1; // First node is always root of MST
-
-        // The MST will have V vertices
         for (int count = 0; count < V - 1; count++) {
-            // Pick thd minimum key vertex from the set of vertices
-            // not yet included in MST
             int u = minKey(key, mstSet);
-
-            // Add the picked vertex to the MST Set
             mstSet[u] = true;
-
-            // Update key value and pi index of the adjacent
-            // vertices of the picked vertex. Consider only those
-            // vertices which are not yet included in MST
-            for (int v = 0; v < V; v++)
-
-                // graph[u][v] is non-zero only for adjacent vertices of m
-                // mstSet[v] is false for vertices not yet included in MST
-                // Update the key only if graph[u][v] is smaller than key[v]
+            for (int v = 0; v < V; v++) {
                 if (graph[u][v] != (-1) && !mstSet[v] && graph[u][v] < key[v]) {
                     pi[v] = u;
                     key[v] = graph[u][v];
                 }
+            }
         }
-
         builderMst(tree, pi, graph);
-        // print the constructed MST
         printMST(pi, graph, tree);
     }
 
@@ -125,22 +97,23 @@ class Prim {
         tree[v2][v1] = weight;
     }
 
-    Stack<Integer> cycleStack= new Stack<>();
+    Stack<Integer> cycleStack = new Stack<>();
     Boolean flag = false;
-    Boolean findCycle(int[][] tree,int v1, int v2, Boolean flag) {
+
+    Boolean findCycle(int[][] tree, int v1, int v2, Boolean flag) {
         if (v1 == v2) {
             return false;
         }
         int count = 0;
         for (int i = 0; i < tree[v1].length; i++) {
-            if (flag){
+            if (flag) {
                 break;
             }
             if (tree[v1][i] != (-1) && v1 != i) {
                 if (!cycleStack.isEmpty() && i == cycleStack.peek()) {
                     continue;
                 }
-                if (cycleStack.isEmpty() || v1 != cycleStack.peek()) { //לבדוק האם מצב שבו צריך להוציא קודרוד מהמחסנית כי הוא לא חלק מהמעגל
+                if (cycleStack.isEmpty() || v1 != cycleStack.peek()) {
                     cycleStack.push(v1);
                 }
                 flag = findCycle(tree, i, v2, flag);
@@ -153,35 +126,35 @@ class Prim {
             } else {
                 count++;
             }
-            if (count == (tree[v1].length - 1)){
+            if (count == (tree[v1].length - 1)) {
                 cycleStack.pop();
             }
         }
         return flag;
     }
 
-    void removeMax(int v1,int v2, int weight){
+    void removeMax(int v1, int v2, int weight) {
         int maxX = v1;
         int maxY = v2;
         int maxWeight = weight;
         int tempX, tempY;
 
         int stackSize = cycleStack.size();
-        for (int i = 0; i < stackSize-1; i++) {
+        for (int i = 0; i < stackSize - 1; i++) {
             tempX = cycleStack.pop();
             tempY = cycleStack.peek();
-            if(tree[tempX][tempY]>maxWeight){
+            if (tree[tempX][tempY] > maxWeight) {
                 maxX = tempX;
-                maxY=tempY;
+                maxY = tempY;
                 maxWeight = tree[tempX][tempY];
             }
         }
-        System.out.println("The edge being removed is: "+ maxX +" - "+maxY+" and it weight is: "+maxWeight);
+        System.out.println("The edge being removed is: " + maxX + " - " + maxY + " and it weight is: " + maxWeight);
         tree[maxX][maxY] = -1;
         tree[maxY][maxX] = -1;
     }
 
-    void q2(int[][] tree ,int v1, int v2, int weight, Boolean flag) {
+    void q2(int[][] tree, int v1, int v2, int weight, Boolean flag) {
         addEdge(tree, v1, v2, weight);
         System.out.println("\n\nThe added edge is:" + v1 + " - " + v2 + ", and it weight is: " + weight);
         //בדיקה
@@ -212,98 +185,69 @@ class Prim {
         System.out.println("Matan Gabso - 208233403");
         System.out.println("************************\n");
 
-
-		/* Let us create the following graph
-		2 3
-		(0)--(1)--(2)
-		| / \ |
-		6| 8/ \5 |7
-		| /	 \ |
-		(3)-------(4)
-			9		 */
         Prim t = new Prim();
         int[][] graph = new int[V][V];
-//                {
-////                        {0, 2, 0, 6, 0},
-////                        {2, 0, 3, 8, 5},
-////                        {0, 3, 0, 0, 7},
-////                        {6, 8, 0, 0, 9},
-////                        {0, 5, 7, 9, 0}};
-//
-////                        {0,6,-1,-1,-1},
-////                        {6,0,7,-1,40},
-////                        {-1,7,0,8,-1},
-////                        {-1,-1,8,0,9},
-////                        {-1,40,-1,9,0}
-//                        {0,6,-1,-1,-1},
-//                        {6,0,5,-1,7},
-//                        {-1,5,0,8,-1},
-//                        {-1,-1,8,0,40},
-//                        {-1,7,-1,40,0}
-//                };
-
         for (int i = 0; i < V; i++) {
             Arrays.fill(graph[i], -1);
-            graph[i][i]=0;
+            graph[i][i] = 0;
         }
 
-        addEdge(graph,1,3,4);
-        addEdge(graph,1,2,1);
-        addEdge(graph,1,4,5);
-        addEdge(graph,2,5,2);
-        addEdge(graph,2,6,8);
-        addEdge(graph,6,12,9);
-        addEdge(graph,11,12,6);
-        addEdge(graph,12,18,8);
-        addEdge(graph,6,7,7);
-        addEdge(graph,7,13,9);
-        addEdge(graph,3,7,10);
-        addEdge(graph,13,14,1);
-        addEdge(graph,3,8,6);
-        addEdge(graph,4,9,9);
-        addEdge(graph,8,9,10);
-        addEdge(graph,8,14,5);
-        addEdge(graph,13,18,2);
-        addEdge(graph,4,10,4);
-        addEdge(graph,9,15,8);
-        addEdge(graph,14,19,5);
-        addEdge(graph,15,19,6);
-        addEdge(graph,15,16,7);
-        addEdge(graph,16,17,8);
-        addEdge(graph,10,16,9);
-        addEdge(graph,5,10,3);
-        addEdge(graph,5,11,11);
-        addEdge(graph,11,17,10);
-        addEdge(graph,18,0,3);
-        addEdge(graph,17,0,7);
-        addEdge(graph,19,0,4);
-        addEdge(graph,3,13,10);
-        addEdge(graph,6,13,8);
-        addEdge(graph,3,6,4);
-        addEdge(graph,1,6,2);
-        addEdge(graph,1,9,9);
-        addEdge(graph,1,8,6);
-        addEdge(graph,3,14,7);
-        addEdge(graph,9,14,3);
-        addEdge(graph,9,19,4);
-        addEdge(graph,9,16,7);
-        addEdge(graph,10,17,10);
-        addEdge(graph,5,17,1);
-        addEdge(graph,11,16,3);
-        addEdge(graph,15,0,6);
-        addEdge(graph,16,0,16);
-        addEdge(graph,11,0,11);
-        addEdge(graph,12,0,4);
-        addEdge(graph,4,18,9);
-        addEdge(graph,6,11,12);
-        addEdge(graph,2,12,14);
+        addEdge(graph, 1, 3, 4);
+        addEdge(graph, 1, 2, 1);
+        addEdge(graph, 1, 4, 5);
+        addEdge(graph, 2, 5, 2);
+        addEdge(graph, 2, 6, 8);
+        addEdge(graph, 6, 12, 9);
+        addEdge(graph, 11, 12, 6);
+        addEdge(graph, 12, 18, 8);
+        addEdge(graph, 6, 7, 7);
+        addEdge(graph, 7, 13, 9);
+        addEdge(graph, 3, 7, 10);
+        addEdge(graph, 13, 14, 1);
+        addEdge(graph, 3, 8, 6);
+        addEdge(graph, 4, 9, 9);
+        addEdge(graph, 8, 9, 10);
+        addEdge(graph, 8, 14, 5);
+        addEdge(graph, 13, 18, 2);
+        addEdge(graph, 4, 10, 4);
+        addEdge(graph, 9, 15, 8);
+        addEdge(graph, 14, 19, 5);
+        addEdge(graph, 15, 19, 6);
+        addEdge(graph, 15, 16, 7);
+        addEdge(graph, 16, 17, 8);
+        addEdge(graph, 10, 16, 9);
+        addEdge(graph, 5, 10, 3);
+        addEdge(graph, 5, 11, 11);
+        addEdge(graph, 11, 17, 10);
+        addEdge(graph, 18, 0, 3);
+        addEdge(graph, 17, 0, 7);
+        addEdge(graph, 19, 0, 4);
+        addEdge(graph, 3, 13, 10);
+        addEdge(graph, 6, 13, 8);
+        addEdge(graph, 3, 6, 4);
+        addEdge(graph, 1, 6, 2);
+        addEdge(graph, 1, 9, 9);
+        addEdge(graph, 1, 8, 6);
+        addEdge(graph, 3, 14, 7);
+        addEdge(graph, 9, 14, 3);
+        addEdge(graph, 9, 19, 4);
+        addEdge(graph, 9, 16, 7);
+        addEdge(graph, 10, 17, 10);
+        addEdge(graph, 5, 17, 1);
+        addEdge(graph, 11, 16, 3);
+        addEdge(graph, 15, 0, 6);
+        addEdge(graph, 16, 0, 16);
+        addEdge(graph, 11, 0, 11);
+        addEdge(graph, 12, 0, 4);
+        addEdge(graph, 4, 18, 9);
+        addEdge(graph, 6, 11, 12);
+        addEdge(graph, 2, 12, 14);
 
-        // Print the solution
         t.primMST(graph);
         System.out.println("\n******************\nAdding an edge that changes the MST:\n******************\n");
-        t.q2(t.tree,3,4,40000000, t.flag);
+        t.q2(t.tree, 3, 4, 40000000, t.flag);
         System.out.println("\n******************\nAdding an edge that doesn't change the MST:\n******************\n");
-        t.q2(t.tree,3,4,1, t.flag);
+        t.q2(t.tree, 3, 4, 1, t.flag);
     }
 
 }
